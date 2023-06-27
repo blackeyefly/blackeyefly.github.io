317559f21537178509164217bfcda3047827a7fe
blackeyefly <rayofdoombloons@gmail.com>
2023-06-27 17:46:01 -0400

    Include vortex 20

--- src/data/extractBossLBs.js
+++ src/data/extractBossLBs.js
@@ -21,6 +21,11 @@ function convertToCSV(objArray) {
 async function main() {
   const bosses = await fetch("https://data.ninjakiwi.com/btd6/bosses/")
   const bossesJson = await bosses.json()
+
+  if (!bossesJson.success) {
+    console.log(bossesJson.reason)
+    return
+  }
   
   bossesJson.body.map(async x => {
     const id = x.id
@@ -30,41 +35,59 @@ async function main() {
     let placementNormal = 1
     let placementElite = 1
 
-    for (let i = 1; i <= 40; i++) {
+    for (let i = 1; i <= 4; i++) {
       const pageNormal = await fetch(`https://data.ninjakiwi.com/btd6/bosses/${id}/leaderboard/standard/1?page=${i}`)
       const pageJsonNormal = await pageNormal.json()
+
+      if (!pageJsonNormal.success) {
+        console.log(pageJsonNormal.reason)
+        return
+      }
 
       const pageElite = await fetch(`https://data.ninjakiwi.com/btd6/bosses/${id}/leaderboard/elite/1?page=${i}`)
       const pageJsonElite = await pageElite.json()
 
-      if (pageJsonNormal && pageJsonElite && pageJsonNormal.body && pageJsonElite.body) {
-        pageJsonNormal.body.map(({
-          displayName,
-          score,
-        }) => {
-          lbNormal.push({
-            placementNormal,
-            displayName,
-            // score: `${Math.floor(score / 60000)}:${score % 60000 / 1000 < 10 ? '0' : ''}${score % 60000 / 1000}`,
-            score
-          })
-          placementNormal++
-        })
-
-        pageJsonElite.body.map(({
-          displayName,
-          score,
-        }) => {
-          lbElite.push({
-            placementElite,
-            displayName,
-            // score: `${Math.floor(score / 60000)}:${score % 60000 / 1000 < 10 ? '0' : ''}${score % 60000 / 1000}`,
-            score
-          })
-          placementElite++
-        })
-        
+      if (!pageJsonElite.success) {
+        console.log(pageJsonElite.reason)
+        return
       }
+
+      pageJsonNormal.body.map(({
+        displayName,
+        score,
+        scoreParts,
+      }) => {
+        if (scoreParts[0] && ["Tiers", "Cash Spent"].includes(scoreParts[0].name)) {
+          var displayedScore = score
+        } else {
+          var displayedScore = `${Math.floor(score / 60000)}:${score % 60000 / 1000 < 10 ? '0' : ''}${score % 60000 / 1000}`
+        }
+        lbNormal.push({
+          placementNormal,
+          displayName,
+          score: displayedScore
+        })
+        placementNormal++
+      })
+
+      pageJsonElite.body.map(({
+        displayName,
+        score,
+        scoreParts,
+      }) => {
+        if (scoreParts[0] && ["Tiers", "Cash Spent"].includes(scoreParts[0].name)) {
+          var displayedScore = score
+        } else {
+          var displayedScore = `${Math.floor(score / 60000)}:${score % 60000 / 1000 < 10 ? '0' : ''}${score % 60000 / 1000}`
+        }
+        lbElite.push({
+          placementElite,
+          displayName,
+          score: displayedScore
+        })
+        placementElite++
+      })
+      await new Promise(r => setTimeout(r, 1000));
     }
 
     fs.writeFile(`${__dirname}/${name}_normal.csv`, convertToCSV(JSON.stringify(lbNormal, null, 1)), err => {if (err) {console.log(err)}})
