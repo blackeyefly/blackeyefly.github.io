83a36ddc6dc69bfdd1d20408f8d9ce2b4be9862e
blackeyefly <rayofdoombloons@gmail.com>
2024-02-07 21:15:22 -0500

    Update prices and add sub paragon

--- src/components/ParagonDegreeCalculator/ParagonDegreeCalculator.tsx
+++ src/components/ParagonDegreeCalculator/ParagonDegreeCalculator.tsx
@@ -20,6 +20,7 @@ const ParagonDegreeCalculator: FC<ParagonDegreeCalculatorProps> = () => {
   const validTowers = [
     TowerType.Dart,
     TowerType.Boomerang,
+    TowerType.Sub,
     TowerType.Buccaneer,
     TowerType.Ace,
     TowerType.Wizard,
